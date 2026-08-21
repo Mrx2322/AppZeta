4a8947927c4e940b0e65b836b25bec7e62d07f86
deivis.ruiz <devmrx23@gmail.com>
2026-08-21 17:49:32 -0500

    avance minimo en los diseños ( mejora continua)

--- app/src/main/java/com/example/appzetar/Menu/EntradasViewHolder.kt
+++ app/src/main/java/com/example/appzetar/Menu/EntradasViewHolder.kt
@@ -2,6 +2,7 @@ package com.example.appzetar.Menu
 
 import android.view.View
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.appzetar.R
 
@@ -15,12 +16,21 @@ class EntradasViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         when(taskEntradas){
             TaskEntradas.ceviche -> {
                 tvEntradasName.text = "Ceviche"
+                divider.setBackgroundColor(
+                    ContextCompat.getColor(divider.context, R.color.black)
+                )
             }
             TaskEntradas.huancaina -> {
                 tvEntradasName.text = "Huancaina"
+                divider.setBackgroundColor(
+                    ContextCompat.getColor(divider.context, R.color.todo_business_category)
+                )
             }
             TaskEntradas.otros -> {
                 tvEntradasName.text = "Otros"
+                divider.setBackgroundColor(
+                    ContextCompat.getColor(divider.context, R.color.todo_background_todo_app)
+                )
             }
         }
     }
