1f739d369d6434e79b05c4b6750a578901a9262e
deivis.ruiz <devmrx23@gmail.com>
2026-08-27 17:59:33 -0500

    Pagos con yape simulacion - agregacion de recursos

--- app/src/main/java/com/example/appzetar/Usuario/ActivityMenuUsuario.kt
+++ app/src/main/java/com/example/appzetar/Usuario/ActivityMenuUsuario.kt
@@ -6,11 +6,9 @@ import android.view.View
 import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.LinearLayout
 import androidx.core.graphics.drawable.toDrawable
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
