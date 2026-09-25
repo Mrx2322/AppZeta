68b094b48a5a9602dc9215dd5d63e2f573645736
deivis.ruiz <devmrx23@gmail.com>
2026-09-25 17:44:53 -0500

    corrigiendo algunos errores de la app

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,8 @@ plugins {
 
     id("com.google.gms.google-services") version "4.5.0" apply false
 }
+buildscript {
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.10")
+    }
+}
